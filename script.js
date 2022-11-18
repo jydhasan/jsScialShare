f49cacb569f4fbcad359eec262e28a5a4a9be7fa
jydhasan <s1910933181@ru.ac.bd>
2022-11-18 10:00:53 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$("#share").jsSocials({
+  url: "http://bit.ly/codegrind",
+  text: "Subscribe to the YouTube Channel",
+  showLabel: true,
+  shares: [
+    "twitter",
+    { share: "facebook", label: "Like our Page" },
+    { share: "whatsapp", label: "Send a Message" },
+  ],
+});
